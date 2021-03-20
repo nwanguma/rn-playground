f339c0fa707dc9afabbfcd4bdf441ec24dcabd64
Tochukwu Nwanguma <tochukwu@cico.ng>
2021-03-20 23:15:55 +0100

    Create deck module

--- src/Deck.js
+++ src/Deck.js
@@ -1,13 +1,28 @@
-import React from "react";
-import { View, Text, Animated, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, PanResponder, Animated } from "react-native";
 
 const Deck = ({ data, renderCard }) => {
+  const position = new Animated.ValueXY();
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => false,
+    onPanResponderMove: (event, gesture) => {
+      position.setValue({ x: gesture.dx, y: gesture.dy });
+    },
+    onPanResponderRelease: () => {},
+  });
+
+  const [panResponderState, setPanResponderState] = useState(panResponder);
+
   const renderCards = () => {
     return data.map((item) => {
       return renderCard(item);
     });
   };
-  return <View>{renderCards()}</View>;
+  return (
+    <Animated.View position={position.getLayout()}>
+      <View {...panResponderState.panHandlers}>{renderCards()}</View>
+    </Animated.View>
+  );
 };
 
 export default Deck;
